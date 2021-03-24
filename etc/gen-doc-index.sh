c0e621438d3d9d97a389d631b3f13b426307c274
Trevor Spiteri <tspiteri@ieee.org>
2021-03-24 09:44:19 +0100

    do not use awk -i in doc index generation

--- etc/gen-doc-index.sh
+++ etc/gen-doc-index.sh
@@ -5,37 +5,41 @@ set -e
 cd public
 cp dev/*/index.html index.html
 
-awk -i inplace '{if ($0 ~ /button>$/) {printf "%s", $0; next}; print}' index.html
-awk -i inplace '{print gensub(/('\''|")\.\.\//, "\\1dev/", "g")}' index.html
-awk -i inplace '{
-gsub(/<\/nav>/, "\013")
-sub(/<div class="block[^\013]*\013/, "\013")
-sub(/<nav class="sub[^\013]*\013/, "")
-gsub(/\013/, "</nav>")
-
-sub(/dev\/[^/]*\/index.html/, "index.html")
-
-gsub(/<\/h1>/, "\013")
-sub(/<h1 class="fqn[^\013]*\013/, "")
-gsub(/\013/, "</h1>")
-
-gsub(/<\/script>/, "\013")
-sub(/<script src="[^"]*main\.js[^\013]*\013/, "")
-gsub(/\013/, "</script>")
-
-print
-}' index.html
-awk -i inplace '{
-if ($0 ~ /<\/h1>/) {
-    print gensub(/(.*<\/h1>).*/, "\\1", 1)
-    sub(/.*<\/h1>/, "")
-    while ($0 !~ /<\/section>/) { getline }
-    while (getline line<"../etc/index-contents.html") { print line }
-    gsub(/<\/section>/, "\013")
-    sub(/^[^\013]*/, "</div>")
-    gsub(/\013/, "</section>")
-    print
-} else {
-    print
+function filter {
+    awk "$1" < index.html > index.html.tmp
+    mv index.html.tmp index.html
 }
-}' index.html
+
+filter '{if ($0 ~ /button>$/) {printf "%s", $0; next}; print}'
+filter '{print gensub(/('\''|")\.\.\//, "\\1dev/", "g")}'
+filter '{
+    gsub(/<\/nav>/, "\013")
+    sub(/<div class="block[^\013]*\013/, "\013")
+    sub(/<nav class="sub[^\013]*\013/, "")
+    gsub(/\013/, "</nav>")
+
+    sub(/dev\/[^/]*\/index.html/, "index.html")
+
+    gsub(/<\/h1>/, "\013")
+    sub(/<h1 class="fqn[^\013]*\013/, "")
+    gsub(/\013/, "</h1>")
+
+    gsub(/<\/script>/, "\013")
+    sub(/<script src="[^"]*main\.js[^\013]*\013/, "")
+    gsub(/\013/, "</script>")
+
+    print
+}'
+
+filter '{
+    if ($0 ~ /<\/h1>/) {
+        print gensub(/(.*<\/h1>).*/, "\\1", 1)
+        sub(/.*<\/h1>/, "")
+        while ($0 !~ /<\/section>/) { getline }
+        while (getline line<"../etc/index-contents.html") { print line }
+        gsub(/<\/section>/, "\013")
+        sub(/^[^\013]*/, "</div>")
+        gsub(/\013/, "</section>")
+    }
+    print
+}'
